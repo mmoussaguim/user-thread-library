fa539b974a08a2fe4148b0ec958936c48a2ff935
sbrouard <sbrouard@b3e6e01d-a3a6-4e66-898a-3440a2f1db5b>
2017-04-26 12:16:48 +0000

    suppression des commentaires innutiles et de la waitqueue

--- thread.c
+++ thread.c
@@ -23,11 +23,7 @@ int id_ref = 0;
 
 //File d'attente de threads prêts
 STAILQ_HEAD(ma_fifo, QueueElt) runqueue;
-			       //int runqueue_is_init = 0;
 
-//File d'attente de threads en attente d'un evt
-STAILQ_HEAD(ma_fifo2, QueueElt) waitqueue;
-				//int waitqueue_is_init = 0;
 
 //Pointeur du thread en exécution
 Thread current_thread; // à initialiser ?
@@ -45,10 +41,7 @@ extern thread_t thread_self(void){
 
 // fonction englobante pour connnaitre la fin d'execution du thread
 void *tmp(void* (*func)(void*), void *arg){
-  //printf("--TEST-- le thread est lance\n");
   void * retval = func(arg);
-  //printf("--TEST-- Le thread a termine\n");
-  //running_thread->retval = retval;
   if (running_thread->state != dead)
     thread_exit(retval);
   return retval;
@@ -58,28 +51,15 @@ void *tmp(void* (*func)(void*), void *arg){
 
 extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
 
-  // fonction englobante pour connnaitre la fin d'execution du thread
-  // !!!!!! NE DOIT PAS ETRE LOCALE !!!!!!
-  /*void *tmp(void* (*func)(void*), void *arg){
-    printf("--TEST-- le thread est lance\n");
-    func(arg);
-    printf("--TEST-- Le thread a termine\n");
-    if (running_thread->state != dead)
-      thread_exit(NULL);
-    return NULL;
-    }*/
-
-    
+      
   //Préparer le contexte
   static ucontext_t uc;
   getcontext(&uc); //TODO : attraper l'erreur 
   uc.uc_stack.ss_size = 64*STACK_SIZE;
   uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
 
-  //printf("--TEST-- create uc: %p -> %p\n", &uc, &uc + sizeof(ucontext_t));
-  
+    
   makecontext(&uc, (void(*)(void))*tmp, 2,func, funcarg);
-  //makecontext(&uc, (void(*)(void))*tmp, 1, funcarg);
   uc.uc_link = NULL; // ?
   
   //Créer un Thread
@@ -88,7 +68,6 @@ extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funca
   new_th->uc = uc;
   new_th->father = NULL;
   new_th->retval = NULL;
-  //new_th->father = running_thread;
   new_th->state = ready;
 
 int valgrind_stackid = VALGRIND_STACK_REGISTER(uc.uc_stack.ss_sp,
@@ -100,10 +79,6 @@ int valgrind_stackid = VALGRIND_STACK_REGISTER(uc.uc_stack.ss_sp,
   new_elt->thread = new_th;
   
   //L'ajouter à la runqueue
-  /*if(!queue_is_init){
-    STAILQ_INIT(&runqueue);
-    queue_is_init = 1;
-    }*/
   STAILQ_INSERT_TAIL(&runqueue, new_elt, next);
   *newthread = new_th;
   return 0;
@@ -112,14 +87,9 @@ int valgrind_stackid = VALGRIND_STACK_REGISTER(uc.uc_stack.ss_sp,
 extern int thread_yield(void){
   if(STAILQ_EMPTY(&runqueue))
     return 0;
-  //printf("--TEST-- %p\n",runqueue.stqh_first);
-  /*if (running_thread == NULL)
-    return -1;*/
-  
+    
   Thread * old_thread = running_thread;
   old_thread->state = ready;
-  //printf("--TEST-- running thread: %p\n",running_thread);
-  // Le thread courant va dans la runqueue 
   QueueElt *run_elt = malloc(sizeof(QueueElt));
   run_elt->thread = thread_self();
 
@@ -131,15 +101,13 @@ extern int thread_yield(void){
   // Passer le premier thread de la runqueue en running
   run_elt = (QueueElt *) STAILQ_FIRST(&runqueue);
   running_thread = run_elt->thread;
-  //printf("--TEST-- running thread: %p\n",running_thread);
-  STAILQ_REMOVE_HEAD(&runqueue, next); //ERREUR type incomplet
+  STAILQ_REMOVE_HEAD(&runqueue, next); 
   free(run_elt);
 
   running_thread->state = running;
   // Recuperer le contexte du nouveau thread courant
   // Et stocker le contexte courrant dans l'ancien thread courant
-  //printf("--TEST-- %p %p\n",&(old_thread->uc),&(running_thread->uc));
-  swapcontext(&(old_thread->uc),&(running_thread->uc)); // fait un segfault (probablement quand il ne reste qu'un seul processus
+  swapcontext(&(old_thread->uc),&(running_thread->uc)); 
   return 0;
 }
 
@@ -154,84 +122,31 @@ extern int thread_yield(void){
 extern int thread_join(thread_t thread, void **retval){
   //le thread courant est passé en father de 'thread', et quitte l'état running
   //run le premier de la fifo
-  // Attention gérer le cas ou le htread a deja terminé
-
-  
-  /*if (thread == NULL || ((Thread*)thread)->state == dead)
-    return 0;
- 
-  printf("--TEST-- join debut\n");
- 
-  running_thread->state = blocked;
-  QueueElt *run_elt = malloc(sizeof(QueueElt));
-  QueueElt *new_elt = malloc(sizeof(QueueElt));
-
-  Thread* my_thread = thread;
-
-  //run_elt = (QueueElt *) STAILQ_FIRST(&runqueue);
-    //STAILQ_REMOVE_HEAD(&runqueue, next);
-  run_elt->thread = thread_self();
-  STAILQ_INSERT_TAIL(&waitqueue, run_elt, next);
-
-  printf("--TEST-- %p\n",run_elt->thread);
-  printf("--TEST-- %p\n",my_thread->father);
-  printf("--TEST-- %p\n",my_thread);
-  //my_thread->father = run_elt->thread; // segfault sur cette ligne -> a cause de my_thread
-  my_thread->father = thread_self();//running_thread;
-  printf("--TEST-- join 2\n");
-  new_elt = (QueueElt *) STAILQ_FIRST(&runqueue);
-  running_thread->state = blocked;
-  running_thread = new_elt->thread;
-  running_thread->state = running;
-  // manque quelques swapcontext()
-  return 0;*/
-
-
-
-  //printf("--TEST-- join debut: %p\n",thread);
-
-  // CC de yield:
-  
+    
   while(!(thread == NULL || ((Thread*)thread)->state == dead)){
 
-    /*if( ((Thread *)thread)->father != NULL)
-      printf("TEST\n");*/
-    
     Thread * son = (Thread *)thread;
     son->father = thread_self();
-    //printf("--TEST-- join jusque la tout va bien\n");
-
+    
         
     Thread * old_thread = running_thread;
     old_thread->state = blocked;
-    //printf("--TEST-- running thread: %p\n",running_thread);
-    // Le thread courant va dans la runqueue 
     QueueElt *run_elt = malloc(sizeof(QueueElt));
     run_elt->thread = thread_self();
     
-    STAILQ_INSERT_TAIL(&waitqueue, run_elt, next); 
-    
-    // Si l'ajout ne s'est pas bien passé, quitter
-    if(STAILQ_EMPTY(&waitqueue))
-      return 1;
     // Passer le premier thread de la runqueue en running
     run_elt = (QueueElt *) STAILQ_FIRST(&runqueue);
     running_thread = run_elt->thread;
-    //printf("--TEST-- running thread: %p\n",running_thread);
-    STAILQ_REMOVE_HEAD(&runqueue, next); //ERREUR type incomplet
+    STAILQ_REMOVE_HEAD(&runqueue, next); 
     free(run_elt);
     
     running_thread->state = running;
     // Recuperer le contexte du nouveau thread courant
     // Et stocker le contexte courrant dans l'ancien thread courant
-    //printf("--TEST-- %p %p\n",&(old_thread->uc),&(running_thread->uc));
-
-    //printf("contexte1: %p\n contecte2: %p\n",&(old_thread->uc),&(running_thread->uc));
-
-    swapcontext(&(old_thread->uc),&(running_thread->uc)); // fait un segfault (probablement quand il ne reste qu'un seul processus
-    //printf("--TEST-- join2\n");    
+    
+    swapcontext(&(old_thread->uc),&(running_thread->uc)); 
   }
-  //printf("--TEST-- join fin\n");
+
   if(retval != NULL)
     *retval = ((Thread*)thread)->retval;
   return 0;
@@ -252,21 +167,15 @@ extern int thread_join(thread_t thread, void **retval){
  * n'est pas correctement implémenté (il ne doit jamais retourner).
  */
 extern void thread_exit(void *retval){
-  //printf("--TEST-- exit debut\n");
   //écrit dans retval avant de free
 
   running_thread->retval = retval;
-  //printf("--TEST11-- exit res=%p\n",running_thread->retval);
   running_thread->state = dead;
   Thread *father = running_thread->father;
   
-    //running_thread = father; // provisoire
-    /*int k = kill(getpid(thread_self()),SIGKILL);	
-      retval = &k;*/
     //mettre le father à la fin de la runqueue
     QueueElt *run_elt = malloc(sizeof(QueueElt));
-    if (father != NULL && father->state != ready){ // segfault sur cette ligne
-      //printf("--TEST-- exit2\n");
+    if (father != NULL && father->state != ready){ 
       father->state = ready;
       run_elt->thread = father;
       STAILQ_INSERT_TAIL(&runqueue, run_elt, next);
@@ -278,13 +187,8 @@ extern void thread_exit(void *retval){
       STAILQ_REMOVE_HEAD(&runqueue, next);
       running_thread = run_elt->thread;
       free(run_elt);
-      //printf("--TEST-- %d\n",running_thread->id);
-      //printf("--TEST-- exit %p\n",&(running_thread->uc));
-      setcontext(&(running_thread->uc)); // Exception  en point flottant
-      //printf("--TEST-- exit jusqu'ici tout va bien\n");
+      setcontext(&(running_thread->uc)); 
     }
-    //printf("--TEST-- exit fin\n");
-  //while(1){} // Pour eviter le warning: la fonction ne doit pas retourner
 }
 
 
@@ -297,8 +201,4 @@ void init(void){
 
   //initialisation de la runqueue
   STAILQ_INIT(&runqueue);
-
-  //initialisation de la waitqueue
-  STAILQ_INIT(&waitqueue);
-
 }
