9c817d205d3b735e75f963133128a9314c46170e
sbrouard <sbrouard@b3e6e01d-a3a6-4e66-898a-3440a2f1db5b>
2017-04-28 12:50:57 +0000

    ajout de la fonction run_other_thread qui lance le premier thread de la run queue, pour eviter la duplication de code

---
+++ run_other_thread.c
@@ -0,0 +1,24 @@
+
+/*
+  Si dans exit passer NULL en argument
+  Retourne 0 si un thread de la runqueue a ete lance
+  1 si la runqueue est vide
+ */
+int run_other_thread(Thread * old_thread){
+
+  if(STAILQ_EMPTY(&runqueue))
+    return 1;
+  
+  run_elt = (QueueElt *) STAILQ_FIRST(&runqueue);
+  running_thread = run_elt->thread;
+  STAILQ_REMOVE_HEAD(&runqueue, next); 
+  free(run_elt);
+
+  //running_thread->state = running;
+  
+  if(old_thread != NULL)
+    swapcontext(&(old_thread->uc),&(running_thread->uc));
+  else 
+    setcontext(&(running_thread->uc));
+  return 0;
+}
